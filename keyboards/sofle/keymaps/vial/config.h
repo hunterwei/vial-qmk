711b4b0651934d38064c18ace72f0575aa8b959b
Hunter Wei <hunterwei@gmail.com>
2023-02-08 11:26:59 -0600

    Added tapping config for Home Row Mods.

--- keyboards/sofle/keymaps/vial/config.h
+++ keyboards/sofle/keymaps/vial/config.h
@@ -26,3 +26,16 @@
 // The four corners
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 5, 3, 8 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 0, 0, 0 }
+
+
+// Configure the global tapping term (default: 200ms)
+#ifdef TAPPING_TERM
+    #undef TAPPING_TERM
+    #define TAPPING_TERM 180
+#endif
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
