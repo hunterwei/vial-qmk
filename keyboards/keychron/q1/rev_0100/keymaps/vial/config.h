50146015fbcf8fa27374b5e6e4f4bfa3c917d9f1
Hunter Wei <hunterwei@gmail.com>
2023-02-15 14:18:02 -0600

    Added custom mod-tap in config.h

--- keyboards/keychron/q1/rev_0100/keymaps/vial/config.h
+++ keyboards/keychron/q1/rev_0100/keymaps/vial/config.h
@@ -11,3 +11,19 @@
 // Disabled as Link Time Optimization does not support
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
+
+// Configure the global tapping term (default: 200ms)
+#ifdef TAPPING_TERM
+    #undef TAPPING_TERM
+    #define TAPPING_TERM 200
+#endif
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Customize Caps Word
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_IDEL_TIMEOUT 3000
